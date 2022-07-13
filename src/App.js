8c948c86223ddb73063d23576fbdc43ea510471e
Lucianopires98 <104905600+Lucianopires98@users.noreply.github.com>
2022-07-13 00:06:57 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Às vezes as coisas demoram, mas acontecem.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
